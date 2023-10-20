5a4f7f1ddfa24cdc791b24cd5119c970de2b1ec4
tarikbouari <tarikbouari@gmail.com>
2023-10-20 04:46:30 +0000

    Create a vaigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
 
 const Navbar = () => {
+
+    // useState is used internally for interative action 
+    // for example in this case we want to make a click event on the menu icon
+    // so we need to define the initial state and write a function to handle the action 
+    
+    // Here we declare the state with a destructing array
+    const [nav, setNav] = useState(true);
+    const handleNav = () => {
+        setNav(!nav);
+    }
+
     return (
-        <div className="text-white flex justify-between h-24 mx-auto px-4 items-center">
-        <h1 className= " text-3xl font-bold text-[#00df9a]">REACT. </h1>
-        <ul className= " flex hidden">
+        // nav container
+        <div className="text-white flex justify-between mx-w-[1024px] h-24 mx-auto px-4 items-center">
+        
+         {/* desktop  menu */}
+        <h1 className="text-3xl font-bold text-[#00df9a]">I.COURSE </h1>
+        {/* desktop navigation is hidden */}
+        <ul className=" hidden md:flex">
             <li className="p-4">About us</li>
             <li className="p-4">Work</li>
             <li className="p-4">Contact</li>
             <li className="p-4">Home</li>
+            <li className="p-4">Get Started</li>
         </ul>
-        {/* mobile device list */}
-  
-        
 
-         <AiOutlineMenu size={20}/>
-         <div className="fixed left-0 top-0 w-[40%] h-full border-r-gray-900 bg-[#000300]">
-         <h1 className= " text-3xl font-bold text-[#00df9a]">REACT. </h1>
-            <ul className=" ">
-                <li className="p-4">About us</li>
-                <li className="p-4">Work</li>
-                <li className="p-4">Contact</li>
-                <li className="p-4">Home</li>
+        {/* mobile device menu */}
+        {/* md in tailwind css is equivalent to min-width(above 768):768px */}
+        <div onClick={handleNav} className="block md:hidden ">
+            
+            {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
+        </div>
+            {/* we use a conditional rendering to display the navigation list when the menu icon is clicked*/ }
+         <div className= { !nav ? "fixed left-0 top-0 w-[40%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500" : 'fixed left-[-100%]' }>
+         
+            <h1 className= " text-3xl font-bold text-[#00df9a] m-4">REACT. </h1>
+            <ul className="uppercase">
+                <li className="p-4 border-b border-gray-100">Home</li>
+                <li className="p-4 border-b border-gray-100">About us</li>
+                <li className="p-4 border-b border-gray-100">Work</li>
+                <li className="p-4 border-b border-gray-100">Contact</li>
+                
+               
             </ul> 
          </div>
         
