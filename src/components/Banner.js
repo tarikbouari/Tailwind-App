8d88ecfd3af159fb9b3b3ec7f53c56fbfd3ac296
tarikbouari <tarikbouari@gmail.com>
2023-10-21 23:54:45 +0000

    Add Banner section

---
+++ src/components/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactTyped from "react-typed";
+const Banner = () => {
+    return(
+        <div className="flex  w-full p-4 text-white ">
+                <div className="i" >
+                    <h1 className="text-3xl">Learn remotely with I.COURSE</h1> 
+                    <h2> Fast and flexible schudle for </h2>
+                    <p>
+                        Get your team in sync, no matter your location Streamline processes. Get acess to more
+                        than 500+ courses.
+                    </p>
+                </div>
+                
+                <div className="bg-[url('/image/learning.png')] p-24 w-full">
+
+
+                </div>
+                
+
+        </div>
+    )
+};
+
+export default Banner;
