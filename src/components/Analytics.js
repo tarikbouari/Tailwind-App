ce29c066a90cfdfc4305637478d3cf4df0811117
tarikbouari <tarikbouari@gmail.com>
2023-10-22 15:18:41 +0000

    Add analytic section

---
+++ src/components/Analytics.js
@@ -0,0 +1,14 @@
+import React from "react";
+import scree from '../images/data.jpg'
+
+const Analytic = () => {
+    return (
+        <div className="container grid grid-cols-2">
+            <img src={scree} alt="analytic screen" className="" />
+            <div>
+                <h1> Hello word</h1>
+            </div>
+        </div>
+    )
+}
+export default Analytic;
